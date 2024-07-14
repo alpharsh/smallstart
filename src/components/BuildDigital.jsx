fc631ec482947af989de079abf06a6ef3c16e1fe
Harsh Tripathi <alpharsh1@gmail.com>
2024-07-15 01:43:46 +0530

    update

--- src/components/BuildDigital.jsx
+++ src/components/BuildDigital.jsx
@@ -14,7 +14,7 @@ const BuildDigital = () => {
           </div>
           <div className='w-full md:w-1/2 flex justify-center items-center'>
             <div className='flex flex-col justify-center'>
-              <h3 className="text-5xl font-bold mb-4 md:text-left">Who We Are ?</h3>
+              <h3 className="md:text-5xl text-4xl font-bold mb-4 md:text-left">Who We Are ?</h3>
               <p className="text-lg text-left text-[#AFAFAF]">
                 At SmallStart, we offer a range of services to help your business succeed. From app development and website development to digital marketing and more, we have everything you need to take your business to the next level.
               </p>
@@ -25,7 +25,7 @@ const BuildDigital = () => {
         <div className="md:w-3/4 mx-auto flex flex-col md:flex-row items-center md:items-center">
           <div className='w-full md:w-1/2 flex justify-center items-center'>
             <div className='flex flex-col justify-center'>
-            <h3 className="text-5xl font-bold mb-4">Built for Your Business</h3>
+            <h3 className="md:text-5xl text-4xl font-bold mb-4">Built for Your Business</h3>
             <p className="text-lg mb-4 text-[#AFAFAF]">
               At SmallStart, we offer a range of services to help your business succeed. From app development and website development to digital marketing and more, we have everything you need to take your business to the next level.
             </p>
