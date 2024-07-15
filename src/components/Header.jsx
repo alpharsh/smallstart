c127b213f9dbd9016a7d15b69eb630e30d5a96ab
Harsh Tripathi <alpharsh1@gmail.com>
2024-07-15 11:55:32 +0530

    hamburger toggle header added for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="bg-black p-4 z-10 flex justify-between items-center sticky top-0 border-b border-gray-800 rounded-bl-3xl rounded-br-3xl">
       <div className="flex items-center">
         <img src="/logo.png" alt="Logo" className="h-10 mr-3" />
       </div>
-      <nav className="flex items-center space-x-4 md:mr-6">
+      <nav className="hidden md:flex items-center space-x-4 md:mr-6">
         <a href="#home" className="text-white">Home</a>
         <a href="#services" className="text-white">Services</a>
         <a href="#contact" className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300">Contact</a>
       </nav>
+      <div className="md:hidden">
+        <button onClick={toggleMenu} className="text-white focus:outline-none">
+          <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'} fa-2x`}></i>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute top-16 left-0 w-full bg-black flex flex-col items-center space-y-4 p-4 md:hidden">
+          <a href="#home" className="text-white">Home</a>
+          <a href="#services" className="text-white" >Services</a>
+          <a href="#contact" className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300">Contact</a>
+        </div>
+      )}
     </header>
   );
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,7 +20,7 @@ const services = [
 
 const Services = () => {
   return (
-    <section className="bg-black text-white py-12 px-6">
+    <section className="bg-black md:mt-20 text-white py-12 px-6">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-4">What We Offer</h2>
         <p className="text-lg mb-8 text-[#AFAFAF] mx-auto md:w-2/3">
@@ -28,7 +28,7 @@ const Services = () => {
         </p>
         <div className="flex flex-wrap justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
           {services.map((service, index) => (
-            <div key={index} className="bg-[#262626] rounded-3xl overflow-hidden shadow-lg w-full md:w-1/4 ">
+            <div key={index} className="bg-[#262626] rounded-3xl overflow-hidden shadow-lg w-full md:w-1/4" style={{border: '1px solid rgba(107, 114, 128, 0.3)'}}>
               <img src={service.image} alt={service.title} className="w-full h-48 object-cover" />
               <div className="p-5">
                 <h3 className="text-xl font-semibold mb-2">{service.title}</h3>
