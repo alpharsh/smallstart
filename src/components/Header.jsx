e247827c3b2b644fe535a450f523710e971c61fa
Harsh Tripathi <alpharsh1@gmail.com>
2024-09-08 15:10:35 +0530

    Refactor Header component to use relative URLs for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,27 +10,53 @@ const Header = () => {
   return (
     <header className="bg-black bg-opacity-40 backdrop-blur-xl p-3 z-20 flex justify-between items-center sticky top-0 border-b border-custom-gray rounded-bl-3xl rounded-br-3xl">
       <div className="flex items-center">
-        <img src="/logo.png" alt="Logo" className="h-9 md:pl-6 pr-3 mr-3" />
+        <a href="/">
+          <img src="/logo.png" alt="Logo" className="h-9 md:pl-6 pr-3 mr-3" />
+        </a>
       </div>
       <nav className="hidden md:flex items-center space-x-4 md:mr-6">
-        <a href="#home" className="text-white">Home</a>
-        <a href="#services" className="text-white">Services</a>
-        <a href="#contact" className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300">Contact</a>
+        <a href="/" className="text-white">
+          Home
+        </a>
+        <a href="/services" className="text-white">
+          Services
+        </a>
+        <a
+          href="/contact"
+          className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300"
+        >
+          Contact
+        </a>
       </nav>
       <div className="md:hidden">
-        <button onClick={toggleMenu} className="text-white focus:outline-none mr-3">
-          <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`} style={{ fontSize: '1.3em' }}></i>
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none mr-3"
+        >
+          <i
+            className={`fas ${isOpen ? "fa-times" : "fa-bars"}`}
+            style={{ fontSize: "1.3em" }}
+          ></i>
         </button>
       </div>
       {isOpen && (
         <div className="absolute top-16 right-2 rounded-xl mx-auto w-3/4 bg-black flex flex-col items-center space-y-4 p-4 md:hidden animate-fadeIn">
-          <a href="#home" className="text-white">Home</a>
-          <a href="#services" className="text-white" >Services</a>
-          <a href="#contact" className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300">Contact</a>
+          <a href="/" className="text-white">
+            Home
+          </a>
+          <a href="/services" className="text-white">
+            Services
+          </a>
+          <a
+            href="/contact"
+            className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300"
+          >
+            Contact
+          </a>
         </div>
       )}
     </header>
   );
-}
+};
 
 export default Header;
