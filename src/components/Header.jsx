073a030b60a1c00ecaf0a841e4b1104f74f09291
Harsh Tripathi <alpharsh1@gmail.com>
2024-09-08 15:36:33 +0530

    Link tag used for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,23 +11,23 @@ const Header = () => {
   return (
     <header className="bg-black bg-opacity-40 backdrop-blur-xl p-3 z-20 flex justify-between items-center sticky top-0 border-b border-custom-gray rounded-bl-3xl rounded-br-3xl">
       <div className="flex items-center">
-        <a href="/">
+        <Link to="/">
           <img src="/logo.png" alt="Logo" className="h-9 md:pl-6 pr-3 mr-3" />
-        </a>
+        </Link>
       </div>
       <nav className="hidden md:flex items-center space-x-4 md:mr-6">
-        <a href="/" className="text-white">
+        <Link to="/" className="text-white">
           Home
-        </a>
-        <a href="/services" className="text-white">
+        </Link>
+        <Link to="/services" className="text-white">
           Services
-        </a>
-        <a
-          href="/contact"
+        </Link>
+        <Link
+          to="/contact"
           className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300"
         >
           Contact
-        </a>
+        </Link>
       </nav>
       <div className="md:hidden">
         <button
@@ -41,18 +42,18 @@ const Header = () => {
       </div>
       {isOpen && (
         <div className="absolute top-16 right-2 rounded-xl mx-auto w-3/4 bg-black flex flex-col items-center space-y-4 p-4 md:hidden animate-fadeIn">
-          <a href="/" className="text-white">
+          <Link to="/" className="text-white">
             Home
-          </a>
-          <a href="/services" className="text-white">
+          </Link>
+          <Link to="/services" className="text-white">
             Services
-          </a>
-          <a
-            href="/contact"
+          </Link>
+          <Link
+            to="/contact"
             className="bg-red-600 hover:bg-red-700 text-white font-bold py-1 px-4 rounded-full shadow-lg transition duration-300"
           >
             Contact
-          </a>
+          </Link>
         </div>
       )}
     </header>
